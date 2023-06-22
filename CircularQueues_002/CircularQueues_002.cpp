e96d921960e7e0173eb2c73c965312e10ceb8a5f
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-06-22 10:45:51 +0700

    jika REAR berada di posisi terakhir array, kembali ke awal array

--- CircularQueues_002/CircularQueues_002.cpp
+++ CircularQueues_002/CircularQueues_002.cpp
@@ -29,7 +29,12 @@ public:
 			REAR = 0;
 		}
 		else {
-
+			//jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
 		}
+		queue_array[REAR] = num;
 	}
 };
