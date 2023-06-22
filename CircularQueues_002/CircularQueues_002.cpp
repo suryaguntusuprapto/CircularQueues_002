f94ceb03b581737e00aab314f2dac11a1c921d1f
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-06-22 11:21:27 +0700

    membuat tampilan menu

--- CircularQueues_002/CircularQueues_002.cpp
+++ CircularQueues_002/CircularQueues_002.cpp
@@ -101,4 +101,18 @@ public:
 
 int main() {
 	Queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement Insert operation" << endl;
+			cout << "2. Implement Delete operation" << endl; 
+			cout << "3. Display value" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+		}
+	}
 }
